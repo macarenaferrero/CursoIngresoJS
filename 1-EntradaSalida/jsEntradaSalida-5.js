c4f33d15dfef0c96dd52681995950d7c905d1993
macarenaferrero <60628955+macarenaferrero@users.noreply.github.com>
2020-02-04 21:18:42 -0300

    Falta de internet

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,28 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	
+    //Declaro variable nombre
+
+var nombre;
+    
+    nombre = document.getElementById("elNombre").value;
+
+    //Declaro variable edad
+
+    var edad;
+
+    edad = document.getElementById("laEdad").value;
+
+    //Muestro mensaje por alert
+
+    alert("Usted se llama " + nombre + " y su edad es " + edad + ".");
+
+    /*Tambien podria haber declarado la variable mensaje
+
+    var mensaje
+
+    mensaje = "Usted se llama " + nombre + " y su edad es " + edad + ".");
+
+    alert(mensaje) */
 }
 

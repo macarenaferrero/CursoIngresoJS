1fe7d06617c10e37cb180b8123012b046653b914
macarenaferrero <60628955+macarenaferrero@users.noreply.github.com>
2020-02-05 13:06:05 -0300

    Completar los incisos.

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,13 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+    var importe;
+    var descuento;
+    var 
+
+    importe = parseFloat(document.getElementById("importe").value);
+    descuento = (importe/(125/100));
+
+    document.getElementById("resultado").value = descuento;
+
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,14 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+    var sueldo;
+    var aumento;
+
+     sueldo = parseFloat(document.getElementById("sueldo").value);
+     aumento = (sueldo*110/100);
+
+     document.getElementById("resultado").value = aumento;
+    
 
 	
 }

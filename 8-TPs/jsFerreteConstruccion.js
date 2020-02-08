f965ae166d6eca1faa127011cde3338a025cf9c4
macarenaferrero <60628955+macarenaferrero@users.noreply.github.com>
2020-02-08 12:36:07 -0300

    Calse sábado

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -5,12 +5,47 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 */
 
 function Rectangulo () 
-{
+{ 
+    //Declaro variables
+var largo;
+var ancho;
+var perimetro;
+var mensaje;
+
+    //Asigno variables
+
+    largo = document.getElementById("Largo").value;
+    largo = parseInt(largo);
+
+    ancho = document.getElementById("Ancho").value;
+    ancho = parseInt(ancho);
+
+    perimetro = (largo*2 + ancho*2)*3;
+    mensaje = ("Debe comprar " + perimetro + " metros de alambre.");
+
+    //Muestro resultado
+
+    alert(mensaje);
 
 }
 function Circulo () 
 {
-	
+	//Declaro variables
+var radio;
+var perimetro;
+var mensaje;
+
+    //Asigno variables
+
+    radio = document.getElementById("Radio").value;
+    radio = parseFloat(radio)
+
+    perimetro = (2*radio*3*3.141592654);
+    mensaje = ("Debe comprar " + perimetro + " metros de alambre.");
+
+    //Muestro resultado
+    
+    alert(mensaje);
 }
 function Materiales () 
 {
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -4,14 +4,81 @@ B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
-{
-	
+{   
+    //Declaro variables
+    var precioUno;
+    var precioDos;
+    var precioTres;
+    var suma;
+    var mensaje;
+
+    //Asigno variables
+
+    precioUno = document.getElementById("PrecioUno").value;
+    precioUno = parseInt(precioUno);
+    precioDos = document.getElementById("PrecioDos").value;
+    precioDos = parseInt(precioDos);
+    precioTres = document.getElementById("PrecioTres").value;
+    precioTres = parseInt(precioTres);
+
+    suma = (precioUno + precioDos + precioTres);
+
+    mensaje = ("La suma de los tres elementos es " + suma + ".");
+
+    //Muestro suma 
+
+    alert (mensaje);
+
 }
 function Promedio () 
 {
-	
+	 //Declaro variables
+     var precioUno;
+     var precioDos;
+     var precioTres;
+     var promedio;
+     var mensaje;
+ 
+     //Asigno variables
+ 
+     precioUno = document.getElementById("PrecioUno").value;
+    precioUno = parseInt(precioUno);
+    precioDos = document.getElementById("PrecioDos").value;
+    precioDos = parseInt(precioDos);
+    precioTres = document.getElementById("PrecioTres").value;
+    precioTres = parseInt(precioTres);
+ 
+     promedio = (precioUno + precioDos + precioTres)/3;
+ 
+     mensaje = ("El promedio de los tres elementos es " + promedio + ".");
+ 
+     //Muestro promedio
+ 
+     alert (mensaje);
 }
 function PrecioFinal () 
 {
-	
+	 //Declaro variables
+     var precioUno;
+     var precioDos;
+     var precioTres;
+     var preciofinal;
+     var mensaje;
+ 
+     //Asigno variables
+ 
+    precioUno = document.getElementById("PrecioUno").value;
+    precioUno = parseInt(precioUno);
+    precioDos = document.getElementById("PrecioDos").value;
+    precioDos = parseInt(precioDos);
+    precioTres = document.getElementById("PrecioTres").value;
+    precioTres = parseInt(precioTres);
+ 
+     preciofinal = (precioUno + precioDos + precioTres)*121/100;
+ 
+     mensaje = ("El precio final de los tres elementos es " + preciofinal + ".");
+ 
+     //Muestro precio final
+ 
+     alert (mensaje);
 }

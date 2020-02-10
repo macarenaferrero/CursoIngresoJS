0dfa2df1c996a7c239c3459692f5e046c850c420
Macarena <macarenaferrero@hotmail.com>
2020-02-10 19:58:54 -0300

    Lunes con Cristian

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -2,6 +2,20 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad;
 
+
+edad = document.getElementById("edad").value;
+
+if (edad == "15") {
+
+var mensaje;
+
+mensaje = ("Niña bonita");
+
+alert(mensaje);
+}
+
+console.log("Resultado de la edad:" + edad);
 
 }//FIN DE LA FUNCIÓN

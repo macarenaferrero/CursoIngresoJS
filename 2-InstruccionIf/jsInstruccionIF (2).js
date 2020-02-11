0b897f983b4279d16e317b04387adf5d5b08f429
Macarena <macarenaferrero@hotmail.com>
2020-02-10 21:29:33 -0300

    Casi termina el lunes

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -2,6 +2,17 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad;
+
+edad = parseInt(document.getElementById("edad").value);
+
+if (edad >= 18) {
+
+    var mensaje;
+
+    mensaje = ("Usted es mayor de edad.");
+    alert(mensaje);
+}
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -2,5 +2,17 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad;
 
-}//FIN DE LA FUNCIÓN
+edad = parseInt(document.getElementById("edad").value);
+
+if (edad >= 18) {
+
+        alert("Usted es mayor de edad.");
+}
+else {
+alert("Usted es menor de edad.")
+
+}
+}
+//FIN DE LA FUNCIÓN

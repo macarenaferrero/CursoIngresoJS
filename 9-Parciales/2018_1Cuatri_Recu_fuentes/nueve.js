94a0bf4698f0db2e941419fb6dc1474c964f145c
alumno <alumno>
2020-03-03 21:56:49 -0300

    Martes por la noche

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,70 @@
-function mostrar()
-{
+function mostrar() {
+    var nombreAnimal;
+    var pesoAnimal;
+    var temperaturaAnimal;
+    var respuesta;
+    var contadorTemp = 0;
+    var flag = 0;
+    var animalMasPesado;
+    var nombreMasPesado;
+    var contadoAnimalesBajoCero = 0;
+    var promedioAnimales;
+    var contadorAnimales = 0;
+    var acumuladorPeso = 0;
+    var maxAnimalBajoCero;
+    var minAnimalBajoCero;
+
+
+    do {
+        nombreAnimal = prompt("Ingrese el nombre del animal.");
+        contadorAnimales++;
+
+        pesoAnimal = parseFloat(prompt("Ingrese el peso del animal (1-1000)."));
+        while (pesoAnimal < 1 || pesoAnimal > 1000 || isNaN(pesoAnimal)) {
+            pesoAnimal = parseFloat(prompt("Número inválido. Reingrese el peso del animal (1-1000)."));
+        }
+        acumuladorPeso = acumuladorPeso + pesoAnimal;
+
+        temperaturaAnimal = parseInt(prompt("Ingrese temperatura del hábitat (-30 - 30)."));
+        while (temperaturaAnimal < -30 || temperaturaAnimal > 30 || isNaN(temperaturaAnimal)) {
+            temperaturaAnimal = parseInt(prompt("Incorrecto. Ingrese temperatura del hábitat (-30 - 30)."));
+        }
+        //PARIDAD________________
+        if (temperaturaAnimal % 2 == 0) {
+            contadorTemp++;
+        }
+        //ANIMAL MÁS PESADO________________
+        if (flag == 0 || pesoAnimal > animalMasPesado) {
+            animalMasPesado = pesoAnimal;
+            nombreMasPesado = nombreAnimal;
+            
+            flag = 1
+        }
+        //ANIMALES BAJO CERO____________________
+        if (temperaturaAnimal < 0) {
+            contadoAnimalesBajoCero++;
+            // MAXIMOS Y MINIMOS________________________
+            
+            if ( contadoAnimalesBajoCero == 1 || temperaturaAnimal > maxAnimalBajoCero) {
+                maxAnimalBajoCero = temperaturaAnimal;
+            }
+            if ( contadoAnimalesBajoCero == 1 || temperaturaAnimal < minAnimalBajoCero) {
+                minAnimalBajoCero = temperaturaAnimal;
+
+            }
+
+        }
+        respuesta = prompt("Desea continuar? De lo contrario ingrese NO.");
+    } while (respuesta != "NO");
+
+    promedioAnimales = acumuladorPeso / contadorAnimales;
+
+    document.write("La cantidad de temperaturas pares es de: " + contadorTemp + "</br>")
+    document.write("El animal más pesado se llama: " + nombreMasPesado + " y pesa: " + animalMasPesado + "</br>")
+    document.write("La cantidad de animales que viven a menos de 0 grados es de: " + contadoAnimalesBajoCero + "</br>")
+    document.write("El promedio del peso de todos los animales: " + promedioAnimales + "</br>")
+    document.write("El peso máximo de los animales bajo cero es: " + maxAnimalBajoCero + "</br>")
+    document.write("El peso mínimo de los animales bajo cero es: " + minAnimalBajoCero + "</br>");
+
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,91 @@
 
 function mostrar()
-{
+/*Bienvenidos.
+Se pide la carga de datos de un contenedor  de alimento canino con un máximo de carga de 1000kg, cargar hasta que
+el cliente quiera o se llene el contenedor.
+se pide:
+-marca
+-kilos por bolsa
+-cantidad de bolsa
+-importe por bolsa
 
+que marca tiene más kilos en el contenedor?
+que marca tiene más bolsas de alimento en el contenedor
+que marca tiene el mayor importe por bolsa de alimento
+el importe y la marca de la bolsa de alimento menos pesada.				
+Mostrar todo con document.write
+*/
+
+{
+    var marcaAlimento;
+    var kilosxBolsa;
+    var cantidadDeBolsas;
+    var importeBolsa;
+    var respuesta;
+    var kilosTotales;
+    var acumuladorKilos=0;
+    var kilosTotalesxMarca;
+    var marcaConMasKilos;
+    var maxBolsas;
+    var maxBolsasxMarca;
+    var flag=0;
+    var maxImporte;
+    var maxImporteMarca;
+    var minImportexBolsa;
+    var minPesoxMarca;
+
+    do {
+        marcaAlimento = prompt("Ingrese marca del alimento.");
+        console.log("Marca alimento: " + marcaAlimento);
+
+        var kilosxBolsa = parseInt(prompt("Ingrese kilos por bolsa"));
+        while (isNaN(kilosxBolsa) || kilosxBolsa < 0 || kilosxBolsa > 1000 ) {
+            kilosxBolsa = parseInt(prompt("Incorrecto. Reingrese los kilos por bolsa"));
+        }
+        console.log("Kilos por bolsa: " + kilosxBolsa);
+        
+
+        cantidadDeBolsas = parseInt(prompt("Ingrese cantidad de bolsas."));
+        while (isNaN(cantidadDeBolsas) || cantidadDeBolsas < 0) {
+            cantidadDeBolsas = parseInt(prompt("Monto incorrecto. Reingrese cantidad de bolsas."));
+        }
+        kilosTotales = kilosxBolsa*cantidadDeBolsas;
+        acumuladorKilos = acumuladorKilos+kilosTotales;
+
+        console.log("Cantidad de bolsas: " + cantidadDeBolsas);
+        console.log("Kilos en el container: " + acumuladorKilos);
+        
+        importeBolsa = parseFloat(prompt("Ingrese importe por bolsa."));
+        while (isNaN(importeBolsa)  || cantidadDeBolsas < 0) {
+            importeBolsa = parseFloat(prompt("Importe incorrecto. Reingrese importe por bolsa."));
+        }
+        
+        console.log("Importe de bolsa: " + importeBolsa);
+
+        if (flag == 0 || kilosTotales > kilosTotalesxMarca) {
+            kilosTotalesxMarca = kilosTotales;
+            marcaConMasKilos = marcaAlimento;
+        }
+        if (flag == 0 || cantidadDeBolsas > maxBolsas) {
+            maxBolsas = cantidadDeBolsas;
+            maxBolsasxMarca = marcaAlimento;
+        }
+        if (flag == 0 || importeBolsa > maxImporte) {
+            maxImporte = importeBolsa;
+            maxImporteMarca = marcaAlimento;
+        }
+        if (flag == 0 || importeBolsa < minImportexBolsa) {
+            minImportexBolsa = importeBolsa;
+            minPesoxMarca = marcaAlimento;
+
+            flag = 1
+        }
+        
+        respuesta = prompt("Desea continuar? De lo contrario ingrese NO")
+    } while (respuesta != "NO"  && acumuladorKilos <= 1000);
+
+    document.write("La marca que tiene más kilos en el contenedor es: " + marcaConMasKilos + "</br>");
+    document.write("La marca que tiene más bolsas de alimento en el contenedor es: " + maxBolsasxMarca + "</br>");
+    document.write("La marca que tiene el mayor importe por bolsa es: " + maxImporteMarca + "</br>");
+    document.write("La marca de la bolsa menos pesada es: " + minPesoxMarca + " y su importe es: " + minImportexBolsa + "</br>");
 }
